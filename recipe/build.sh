5752f2289efd50da93f13332a461a50e23ade705
Lisandro Dalcin <dalcinl@gmail.com>
2017-10-08 17:10:37 +0300

    Rerender and update recipe
    - Single Makefile.inc (requires GNU Make).
    - Do not define `SCOTCH_PTHREAD`. Number of threads is a compile-time
      setting and defaults to one. Client code is forced to call
      `MPI_Init_thread()` requesting `MPI_THREAD_MULTIPLE` support.
    - Build/check/install/test scotch, then ptscotch. This should simplify
      a future split in two, with ptscotch depending on scotch and providing
      only the parallel headers and libraries.
    - Add trivial patch for a missing function prototype.
    - Add pthread patch from https://bitbucket.org/petsc/pkg-scotch.
    - Bump build number.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,49 @@
 #!/bin/sh
 
+cp $RECIPE_DIR/Makefile.inc src/Makefile.inc
+
+# build
 cd src/
-
-if [ `uname` == "Darwin" ]; then
-    cp $RECIPE_DIR/Makefile.inc.i686_mac_darwin10 Makefile.inc
-else
-  cp $RECIPE_DIR/Makefile.inc.x86-64_pc_linux2 Makefile.inc
-  sed -i 's#-l$(SCOTCHLIB)errexit#-l$(SCOTCHLIB)errexit -lm#g' esmumps/Makefile
-fi
-
-make ptesmumps | tee make.log 2>&1
+make esmumps 2>&1 | tee -a make.log
 make check
 cd ..
-
-# install.
+# install
 mkdir -p $PREFIX/lib/
 cp lib/* $PREFIX/lib/
 mkdir -p $PREFIX/bin/
 cp bin/* $PREFIX/bin/
 mkdir -p $PREFIX/include/
 cp include/* $PREFIX/include/
+
+if [ "$PKG_NAME" == "ptscotch" ]
+then
+
+export HYDRA_LAUNCHER=fork
+export MPIEXEC=mpiexec
+if [[ "$(uname)" == "Linux" ]]; then
+  # skip mpiexec tests on Linux due to conda-forge bug:
+  # https://github.com/conda-forge/conda-smithy/pull/337
+  export MPIEXEC="echo SKIPPING $MPIEXEC"
+fi
+if [[ "$(uname)" == "Darwin" ]]; then
+  # the following test fails with "write error"
+  # on Travis-CI during 'make ptcheck'
+  # $ mpiexec -n 4 ./test_scotch_dgraph_redist data/bump.grf
+  export MPIEXEC="echo SKIPPING $MPIEXEC"
+fi
+
+# build
+cd src/
+make ptesmumps 2>&1 | tee -a make.log
+make ptcheck EXECP="$MPIEXEC -n 4"
+cd ..
+# install.
+mkdir -p $PREFIX/lib/
+cp lib/libpt* $PREFIX/lib/
+mkdir -p $PREFIX/bin/
+cp bin/dg* $PREFIX/bin/
+mkdir -p $PREFIX/include/
+cp include/ptscotch*.h $PREFIX/include/
+cp include/parmetis.h  $PREFIX/include/
+
+fi

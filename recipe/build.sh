44fc2d8401ea8e40fe2b308bf127a76153dc4f43
Min RK <benjaminrk@gmail.com>
2017-10-09 13:19:07 +0200

    get Makefile.inc, build.sh from ptscotch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,36 +1,58 @@
-#!/bin/sh
+#!/bin/bash
 
-env | sort
 cp $RECIPE_DIR/Makefile.inc src/Makefile.inc
 
-export CFLAGS="${CFLAGS} -I${PREFIX}/include -O3 -DIDXSIZE64 -Drestrict=__restrict -DCOMMON_FILE_COMPRESS_GZ -DCOMMON_RANDOM_FIXED_SEED -DSCOTCH_RENAME -DCOMMON_PTHREAD -DCOMMON_PTHREAD_BARRIER"
-export LDFLAGS="${LDFLAGS} -lz -lm -pthread"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include -DIDXSIZE64 -DSCOTCH_RENAME -Drestrict=__restrict -DCOMMON_FILE_COMPRESS_GZ -DCOMMON_RANDOM_FIXED_SEED -DCOMMON_PTHREAD"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lz -lm -pthread"
 
-if [[ "$(uname)" == "Darwin" ]]; then
+if [[ $(uname) == "Darwin" ]]; then
     export CFLAGS="${CFLAGS} -DCOMMON_PTHREAD_BARRIER -DCOMMON_TIMING_OLD"
-    export CLIBFLAGS="${CLIBFLAGS} -fPIC"
 else
     export LDFLAGS="${LDFLAGS} -lrt"
 fi
 
-if [[ "${PKG_NAME}" == "scotch" ]]; then
-    # serial build
+if [[ "$PKG_NAME" == "scotch" ]]; then
+    # allow non-mpi scotch to use threads
     export CFLAGS="${CFLAGS} -DSCOTCH_PTHREAD"
-    LIBNAME=esmumps
-else
-    # parallel build
-    LIBNAME=ptesmumps
 fi
 
+# build
 cd src/
-make ${LIBNAME} | tee make.log 2>&1
+make esmumps 2>&1 | tee -a make.log
 make check
 cd ..
 
-# install.
+# install
 mkdir -p $PREFIX/lib/
 cp lib/* $PREFIX/lib/
 mkdir -p $PREFIX/bin/
 cp bin/* $PREFIX/bin/
 mkdir -p $PREFIX/include/
 cp include/* $PREFIX/include/
+
+if [[ "$PKG_NAME" == "ptscotch" ]]; then
+
+    export HYDRA_LAUNCHER=fork
+    export MPIEXEC="bash ${RECIPE_DIR}/mpiexec.sh"
+
+    if [[ "$(uname)" == "Linux" ]]; then
+      # skip mpiexec tests on Linux due to conda-forge bug:
+      # https://github.com/conda-forge/conda-smithy/pull/337
+      export MPIEXEC="echo SKIPPING $MPIEXEC"
+    fi
+
+    # build
+    cd src/
+    make ptesmumps 2>&1 | tee -a make.log
+    make ptcheck EXECP="$MPIEXEC -n 4"
+    cd ..
+
+    # install
+    mkdir -p $PREFIX/lib/
+    cp lib/libpt* $PREFIX/lib/
+    mkdir -p $PREFIX/bin/
+    cp bin/dg* $PREFIX/bin/
+    mkdir -p $PREFIX/include/
+    cp include/ptscotch*.h $PREFIX/include/
+    cp include/parmetis.h  $PREFIX/include/
+fi

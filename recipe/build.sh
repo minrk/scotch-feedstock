9c0e08b6caf5b92a0ba87dc20d0cf314d4baefbf
Min RK <benjaminrk@gmail.com>
2017-10-09 10:31:07 +0200

    single Makefile.inc, build.sh for scotch and ptscotch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
-cp $RECIPE_DIR/Makefile.inc.common src/Makefile.inc.common
-cp $RECIPE_DIR/Makefile.inc.$(uname) src/Makefile.inc
+env | sort
+cp $RECIPE_DIR/Makefile.inc src/Makefile.inc
+
+export CFLAGS="${CFLAGS} -I${PREFIX}/include -O3 -DIDXSIZE64 -Drestrict=__restrict -DCOMMON_FILE_COMPRESS_GZ -DCOMMON_RANDOM_FIXED_SEED -DSCOTCH_RENAME -DCOMMON_PTHREAD -DCOMMON_PTHREAD_BARRIER"
+export LDFLAGS="${LDFLAGS} -lz -lm -pthread"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    export CFLAGS="${CFLAGS} -DCOMMON_PTHREAD_BARRIER -DCOMMON_TIMING_OLD"
+    export CLIBFLAGS="${CLIBFLAGS} -fPIC"
+else
+    export LDFLAGS="${LDFLAGS} -lrt"
+fi
+
+if [[ "${PKG_NAME}" == "scotch" ]]; then
+    # serial build
+    export CFLAGS="${CFLAGS} -DSCOTCH_PTHREAD"
+    LIBNAME=esmumps
+else
+    # parallel build
+    LIBNAME=ptesmumps
+fi
 
 cd src/
-make -j ${NUM_CPUS} esmumps | tee make.log 2>&1
+make -j ${NUM_CPUS} ${LIBNAME} | tee make.log 2>&1
 make check
 cd ..
 

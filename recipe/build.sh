64e9e2ccc73a816c4661b5ea60060a7e806fb150
Min RK <benjaminrk@gmail.com>
2017-10-10 10:02:07 +0200

    move flags logic to build.sh from Makefile

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,19 @@
 
 cp $RECIPE_DIR/Makefile.inc src/Makefile.inc
 
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include -DIDXSIZE64 -DSCOTCH_RENAME -Drestrict=__restrict -DCOMMON_FILE_COMPRESS_GZ -DCOMMON_RANDOM_FIXED_SEED -DCOMMON_PTHREAD"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lz -lm -pthread"
+
+if [[ $(uname) == "Darwin" ]]; then
+  export CFLAGS="${CFLAGS} -DCOMMON_PTHREAD_BARRIER -DCOMMON_TIMING_OLD"
+else
+  export LDFLAGS="${LDFLAGS} -lrt"
+fi
+
 if [ "$PKG_NAME" == "scotch" ]
 then
+
+export CFLAGS="${CFLAGS} -DSCOTCH_PTHREAD"
 
 # build
 cd src/
